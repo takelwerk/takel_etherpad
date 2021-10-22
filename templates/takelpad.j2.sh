9a67ef6903a4365ae199ed3822ee4ec62a392aa5
Stephan <stephan@takelwerk.net>
2021-10-22 13:52:24 +0200

    Update hg repos

--- templates/takelpad.j2.sh
+++ templates/takelpad.j2.sh
@@ -11,31 +11,53 @@ echo "$HEADER | for details run: vagrant ssh -c takelpad"
 IP_ETHERPAD=$(ip --json address | jq -r '.[-1].addr_info[0].local')
 echo "takelpad ip address: $IP_ETHERPAD"
 
+echo
+echo "example pad urls (password, de):"
+gopass pwgen --ambiguous --one-per-line --xkcd --xkcdsep / --xkcdlang de | parallel echo https://$IP_ETHERPAD/etherpad/p/{/}
+echo
+echo "example pad urls (password, en):"
+gopass pwgen --ambiguous --one-per-line --xkcd --xkcdsep / --xkcdlang en | parallel echo https://$IP_ETHERPAD/etherpad/p/{/}
+echo
+echo "example pad urls (passphrase, de):"
+gopass pwgen --one-per-line --xkcd --xkcdsep - --xkcdlang de | parallel echo https://$IP_ETHERPAD/etherpad/p/{}
+echo
+echo "example pad urls (passphrase, en):"
+gopass pwgen --one-per-line --xkcd --xkcdsep - --xkcdlang en | parallel echo https://$IP_ETHERPAD/etherpad/p/{}
+
 if [ "$1" != "--summary" ]; then
-  IP_ADDRESSES=$(ip --json address | jq -r '.[].addr_info[0].local')
+  IP_ADDRESSES=$(ip --json address | jq -r '.[].addr_info[0].local' | grep -v null)
   ETHERPAD_ADMIN_PASSWORD=$(sudo {{ takel_etherpad_bin }}/etherpad-admin-password-get)
   MYSQL_ETHERPAD_USER=$(sudo grep user {{ takel_etherpad_home }}/.my.cnf | sed -e 's/.*"\([^"]*\)"/\1/')
   MYSQL_ETHERPAD_PASSWORD=$(sudo grep password {{ takel_etherpad_home }}/.my.cnf | sed -e 's/.*"\([^"]*\)"/\1/')
   MYSQL_ROOT_USER=$(sudo grep user /root/.my.cnf | sed -e 's/.*"\([^"]*\)"/\1/')
   MYSQL_ROOT_PASSWORD=$(sudo grep password /root/.my.cnf | sed -e 's/.*"\([^"]*\)"/\1/')
+  SSL_FINGERPRINT=$(openssl x509 -in /etc/ssl/certs/ssl.pem -noout -fingerprint)
 
   echo
   echo "box ip addresses:"
   echo $IP_ADDRESSES | tr " " "\n"
   echo
   echo "etherpad admin user: admin"
-  echo "etherpad admin password: $ETHERPAD_ADMIN_PASSWORD"
+  echo "etherpad admin pass: $ETHERPAD_ADMIN_PASSWORD"
   echo
   echo "mysql etherpad user: $MYSQL_ETHERPAD_USER"
-  echo "mysql etherpad password: $MYSQL_ETHERPAD_PASSWORD"
+  echo "mysql etherpad pass: $MYSQL_ETHERPAD_PASSWORD"
   echo
   echo "mysql root user: $MYSQL_ROOT_USER"
-  echo "mysql root password: $MYSQL_ROOT_PASSWORD"
-  echo
-  echo "start box: vagrant up"
-  echo "stop box: vagrant halt"
-  echo "destroy box: vagrant destroy"
-  echo
-  echo "login user: vagrant ssh"
-  echo "login root: vagrant ssh -c 'sudo su -'"
+  echo "mysql root pass: $MYSQL_ROOT_PASSWORD"
+
+  if [ -d "/home/vagrant" ]; then
+
+    echo
+    echo "start box:   vagrant up"
+    echo "stop box:    vagrant halt"
+    echo "destroy box: vagrant destroy"
+    echo "update box:  vagrant box update --box takelwerk/takelpad"
+    echo
+    echo "login user: vagrant ssh"
+    echo "login root: vagrant ssh -c 'sudo su -'"
+    echo
+    echo "ssl fingerprint: $SSL_FINGERPRINT"
+    echo "ssl check: openssl s_client -connect $IP_ETHERPAD:443  < /dev/null 2>/dev/null | openssl x509 -fingerprint -noout -in /dev/stdin"
+  fi
 fi
